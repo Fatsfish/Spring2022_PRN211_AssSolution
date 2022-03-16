90ff5e0b495d86d8cbbf13a6d3c2fb46cb9e77fe
James Nguyễn <73916167+Fatsfish@users.noreply.github.com>
2022-03-16 14:20:51 +0700

    Add controller template with direct access business object

---
+++ Ass03Solution_SE1505_NguyenTuanKhai/eStore/Controllers/MemberController.cs
@@ -0,0 +1,149 @@
+﻿using BusinessObject;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eStore.Controllers
+{
+    public class MemberController : Controller
+    {
+        private readonly FStoreDBAssignmentContext _context;
+
+        public MemberController(FStoreDBAssignmentContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Members
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Members.ToListAsync());
+        }
+
+        // GET: Members/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var Member = await _context.Members
+                .FirstOrDefaultAsync(m => m.MemberId == id);
+            if (Member == null)
+            {
+                return NotFound();
+            }
+
+            return View(Member);
+        }
+
+        // GET: Members/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Members/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("MemberId,CompanyName,City,Country,Password,Email")] Member Member)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(Member);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(Member);
+        }
+
+        // GET: Members/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var Member = await _context.Members.FindAsync(id);
+            if (Member == null)
+            {
+                return NotFound();
+            }
+            return View(Member);
+        }
+
+        // POST: Members/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("MemberId,CompanyName,City,Country,Password,Email")] Member Member)
+        {
+            if (id != Member.MemberId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(Member);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!MemberExists(Member.MemberId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(Member);
+        }
+
+        // GET: Members/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var Member = await _context.Members
+                .FirstOrDefaultAsync(m => m.MemberId == id);
+            if (Member == null)
+            {
+                return NotFound();
+            }
+
+            return View(Member);
+        }
+
+        // POST: Members/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var Member = await _context.Members.FindAsync(id);
+            _context.Members.Remove(Member);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool MemberExists(int id)
+        {
+            return _context.Members.Any(e => e.MemberId == id);
+        }
+    }
+}
